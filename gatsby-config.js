a9011f04f39ee3b14f5ec13435a31c930546bf67
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-13 18:20:21 +0000

    Added bio component

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,22 @@ module.exports = {
         this for almost a year.`,
       },
     ],
+    biography: {
+      statement: `Mission driven react developer with a passion to create apps that
+      allows you to express your ideas.`,
+      missionStatement: ` Enthusiastic programmer looking to expand his skills with the right
+      front-end react developer position. I have a Theoretical knowledge of
+      the fundamentals HTML, CSS and JS. I specialise in react development,
+      graphql, redux, redux thunk and reselect. I'm seeking to further
+      develop my front end skills and be part of a team that can accelerate
+      my development.`,
+      subStatement: `I started with web development during my years at univeristy. I made
+      websites and 2D games as a hobby. In my 3rd year I was responsible for
+      creating programs for neuroscience lab projects. Recently I started to
+      pursue web development as a career because it is something I enjoy. I
+      enrolled with coursera and completed courses, im currently updating my
+      freecodecamp projects and improving my skills with Udemy courses.`,
+    },
     socials: [
       {
         link: "https://github.com/danielphilipjohnson",
---
+++ src/component/Biography.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Biography() {
+  return (
+    <div class="bio">
+      <div class="bio-content">
+        <h3 class="text-secondary">Biography:</h3>
+        <h4 class="mission">
+          Mission driven react developer with a passion to create apps that
+          allows you to express your ideas.
+        </h4>
+
+        <p>
+          Enthusiastic programmer looking to expand his skills with the right
+          front-end react developer position. I have a Theoretical knowledge of
+          the fundamentals HTML, CSS and JS. I specialise in react development,
+          graphql, redux, redux thunk and reselect. I'm seeking to further
+          develop my front end skills and be part of a team that can accelerate
+          my development.
+        </p>
+        <p>
+          I started with web development during my years at univeristy. I made
+          websites and 2D games as a hobby. In my 3rd year I was responsible for
+          creating programs for neuroscience lab projects. Recently I started to
+          pursue web development as a career because it is something I enjoy. I
+          enrolled with coursera and completed courses, im currently updating my
+          freecodecamp projects and improving my skills with Udemy courses.
+        </p>
+        <p>
+          Current Focus:
+          <span class="text-secondary"> Accessibility </span>
+          //
+          <span class="text-secondary"> React </span>
+          //
+          <span class="text-secondary"> Python </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Biography;
