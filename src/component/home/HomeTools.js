ddee3326ad21482c8962cd5d6194e122c4ed97a9
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-13 11:56:29 +0000

    Display speciality data to the view via map!

--- src/component/home/HomeTools.js
+++ src/component/home/HomeTools.js
@@ -18,52 +18,15 @@ function HomeTools() {
         <div className="bottom-line"></div>
 
         <div className="specials">
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/danielphilipjohnson/Free-Code-Camp-Portfolio-2018-2019/master/1.Responsive-Web-Design-Certification/5.Personal-Portfolio-Webpage/img/logos/js.png"
-              alt="js"
-            />
-            <h3>JavaScript</h3>
-            <p>
-              I have been using JavaScript for over two years which I have built
-              numerous projects and web apps.
-            </p>
-          </div>
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/danielphilipjohnson/Free-Code-Camp-Portfolio-2018-2019/master/1.Responsive-Web-Design-Certification/5.Personal-Portfolio-Webpage/img/logos/react.svg"
-              alt="react"
-            />
-            <h3>React</h3>
-            <p>
-              I have built numerous projects with React which include a Markdown
-              Previewer, Drum Machine and a portfolio. Currently I'am finishing
-              of an amazon clone.
-            </p>
-          </div>
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/danielphilipjohnson/Free-Code-Camp-Portfolio-2018-2019/master/1.Responsive-Web-Design-Certification/5.Personal-Portfolio-Webpage/img/logos/Redux1.png"
-              alt="Redux"
-            />
-            <h3>Redux</h3>
-            <p>
-              I use this as my current state manager. I have over a years
-              experience.
-            </p>
-          </div>
-
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/danielphilipjohnson/Free-Code-Camp-Portfolio-2018-2019/master/1.Responsive-Web-Design-Certification/5.Personal-Portfolio-Webpage/img/logos/GraphQL.png"
-              alt="GraphQL"
-            />
-            <h3>GraphQL</h3>
-            <p>
-              I use GraphQL as query language for my API's. I have been using
-              this for almost a year.
-            </p>
-          </div>
+          {site.siteMetadata.specialities.map((skill) => {
+            return (
+              <div key={skill.heading}>
+                <img src={skill.src} alt={skill.heading} />
+                <h3>{skill.heading}</h3>
+                <p>{skill.description}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
