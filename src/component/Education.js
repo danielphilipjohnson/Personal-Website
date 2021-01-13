475cc17c41d21b00fd9bb609a69ddc44244d34df
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-13 12:57:02 +0000

    Updated styling for education card and made minor improvements

--- src/component/Education.js
+++ src/component/Education.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./education.css";
+
 import atlassian from "../images/course-logos/atlassian.webp";
 import google from "../images/course-logos/google.png";
 import hong from "../images/course-logos/hong.png";
@@ -12,215 +14,220 @@ function Education() {
   return (
     <div id="education" className="bio-education tabcontent">
       <h2 className="text-secondary">Education</h2>
-
-      <div className="education-grid">
-        <div className="card course">
-          <img className="course-logo" src={uci} alt="uci" />
-          <div className="course-content">
-            <h3>Project Management: The Basics for Success</h3>
-            <p className="info">University of California</p>
-            <p className="grade">
-              <span className="date">Issued October 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.coursera.org/account/accomplishments/specialization/certificate/WKJ9ETHJFF7S"
-            >
-              <i className="fas fa-graduation-cap"></i>
-              View Certificate
-            </a>
+      <div className="container">
+        <div className="education-grid">
+          <div className="course">
+            <img className="course-logo" src={uci} alt="uci" />
+            <div className="course-content">
+              <h3>Project Management: The Basics for Success</h3>
+              <p className="info">University of California</p>
+              <p className="grade">
+                <span className="date">Issued October 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.coursera.org/account/accomplishments/specialization/certificate/WKJ9ETHJFF7S"
+              >
+                <i className="fas fa-graduation-cap"></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="card course">
-          <img className="course-logo" src={udemy} alt="udemy" />
-          <div className="course-content">
-            <h3> Modern HTML & CSS From The Beginning</h3>
-            <p className="info">Udemy</p>
-            <p className="grade">
-              <span className="date">Issued September 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.udemy.com/certificate/UC-49df60d0-c960-433b-a62b-8bbaa5cd3846/"
-            >
-              <i className="fas fa-graduation-cap"></i>
-              View Certificate
-            </a>
+          <div className="card course">
+            <img className="course-logo" src={udemy} alt="udemy" />
+            <div className="course-content">
+              <h3> Modern HTML & CSS From The Beginning</h3>
+              <p className="info">Udemy</p>
+              <p className="grade">
+                <span className="date">Issued September 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.udemy.com/certificate/UC-49df60d0-c960-433b-a62b-8bbaa5cd3846/"
+              >
+                <i className="fas fa-graduation-cap"></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="card course">
-          <img className="course-logo" src={hong} alt="hong kong University" />
-          <div className="course-content">
-            <h3>Full-Stack Web Development with React</h3>
-            <p className="info">University of Hong Kong</p>
-            <p className="grade">
-              <span className="date">Issued Aug 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.coursera.org/account/accomplishments/specialization/certificate/WKJ9ETHJFF7S"
-            >
-              <i className="fas fa-graduation-cap"></i>
-              View Certificate
-            </a>
+          <div className="card course">
+            <img
+              className="course-logo"
+              src={hong}
+              alt="hong kong University"
+            />
+            <div className="course-content">
+              <h3>Full-Stack Web Development with React</h3>
+              <p className="info">University of Hong Kong</p>
+              <p className="grade">
+                <span className="date">Issued Aug 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.coursera.org/account/accomplishments/specialization/certificate/WKJ9ETHJFF7S"
+              >
+                <i className="fas fa-graduation-cap"></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="course">
-          <img
-            className="course-logo"
-            src={michigan}
-            alt="michigan University"
-          />
-          <div className="course-content">
-            <h3>Web Applications for Everybody</h3>
-            <p className="info">University of Michigan</p>
-            <p className="grade">
-              <span className="date">June 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.coursera.org/account/accomplishments/specialization/certificate/EWWJFR4LJM7J"
-            >
-              <i
-                className="fas
+          <div className="course">
+            <img
+              className="course-logo"
+              src={michigan}
+              alt="michigan University"
+            />
+            <div className="course-content">
+              <h3>Web Applications for Everybody</h3>
+              <p className="info">University of Michigan</p>
+              <p className="grade">
+                <span className="date">June 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.coursera.org/account/accomplishments/specialization/certificate/EWWJFR4LJM7J"
+              >
+                <i
+                  className="fas
               fa-graduation-cap"
-              ></i>
-              View Certificate
-            </a>
+                ></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="course">
-          <img
-            className="course-logo"
-            src={michigan}
-            alt="michigan University"
-          />
-          <div className="course-content">
-            <h3>Python for Everybody</h3>
-            <p className="info">University of Michigan</p>
-            <p className="grade">
-              <span className="date">March 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.coursera.org/account/accomplishments/specialization/certificate/8UFSG6W97PMY"
-            >
-              <i
-                className="fas
+          <div className="course">
+            <img
+              className="course-logo"
+              src={michigan}
+              alt="michigan University"
+            />
+            <div className="course-content">
+              <h3>Python for Everybody</h3>
+              <p className="info">University of Michigan</p>
+              <p className="grade">
+                <span className="date">March 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.coursera.org/account/accomplishments/specialization/certificate/8UFSG6W97PMY"
+              >
+                <i
+                  className="fas
               fa-graduation-cap"
-              ></i>
-              View Certificate
-            </a>
+                ></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="course">
-          <img className="course-logo" src={atlassian} alt="atlassian" />
-          <div className="course-content">
-            <h3>Version Control with Git</h3>
-            <p className="info">Atlassian</p>
-            <p className="grade">
-              <span className="date">March 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.coursera.org/account/accomplishments/certificate/WCMWFJX9QAXV"
-            >
-              <i className="fas fa-graduation-cap"></i>
-              View Certificate
-            </a>
+          <div className="course">
+            <img className="course-logo" src={atlassian} alt="atlassian" />
+            <div className="course-content">
+              <h3>Version Control with Git</h3>
+              <p className="info">Atlassian</p>
+              <p className="grade">
+                <span className="date">March 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.coursera.org/account/accomplishments/certificate/WCMWFJX9QAXV"
+              >
+                <i className="fas fa-graduation-cap"></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="course">
-          <img className="course-logo" src={google} alt="google course" />
-          <div className="course-content">
-            <h3>Google IT Automation with Python Specialization</h3>
-            <p className="info">Google</p>
-            <p className="grade">
-              <span className="date">March 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.coursera.org/account/accomplishments/specialization/certificate/EEGGHB96R3KQ"
-            >
-              <i className="fas fa-graduation-cap"></i>
-              View Certificate
-            </a>
+          <div className="course">
+            <img className="course-logo" src={google} alt="google course" />
+            <div className="course-content">
+              <h3>Google IT Automation with Python Specialization</h3>
+              <p className="info">Google</p>
+              <p className="grade">
+                <span className="date">March 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.coursera.org/account/accomplishments/specialization/certificate/EEGGHB96R3KQ"
+              >
+                <i className="fas fa-graduation-cap"></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="course">
-          <img
-            className="course-logo"
-            src={michigan}
-            alt="michigan university"
-          />
-          <div className="course-content">
-            <h3> Web Design for Everybody</h3>
-            <p className="info">University of Michigan</p>
-            <p className="grade">
-              <span className="date">May 2020</span>
-            </p>
-            <a
-              className="btn-certificate"
-              href="https://www.coursera.org/account/accomplishments/specialization/certificate/2A36K26P5QER"
-            >
-              <i
-                className="fas
+          <div className="course">
+            <img
+              className="course-logo"
+              src={michigan}
+              alt="michigan university"
+            />
+            <div className="course-content">
+              <h3> Web Design for Everybody</h3>
+              <p className="info">University of Michigan</p>
+              <p className="grade">
+                <span className="date">May 2020</span>
+              </p>
+              <a
+                className="btn-certificate"
+                href="https://www.coursera.org/account/accomplishments/specialization/certificate/2A36K26P5QER"
+              >
+                <i
+                  className="fas
               fa-graduation-cap"
-              ></i>
-              View Certificate
-            </a>
+                ></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="course">
-          <img
-            className="course-logo"
-            src={plymouth}
-            alt="plymouth university"
-          />
-          <div className="course-content">
-            <h3>BSc Psychology</h3>
-            <p className="info">University of Plymouth</p>
+          <div className="course">
+            <img
+              className="course-logo"
+              src={plymouth}
+              alt="plymouth university"
+            />
+            <div className="course-content">
+              <h3>BSc Psychology</h3>
+              <p className="info">University of Plymouth</p>
 
-            <p className="grade">
-              <span className="date">April 2014</span>
-            </p>
-            <a className="btn-certificate" href="#">
-              <i
-                className="fas
+              <p className="grade">
+                <span className="date">April 2014</span>
+              </p>
+              <a className="btn-certificate" href="#">
+                <i
+                  className="fas
               fa-graduation-cap"
-              ></i>
-              View Certificate
-            </a>
+                ></i>
+                View Certificate
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="course">
-          <img
-            className="course-logo"
-            src={plymouth}
-            alt="plymouth university"
-          />
-          <div className="course-content">
-            <h3>HND Applied Psychology</h3>
-            <p className="info">University of Plymouth</p>
-            <p className="grade">
-              <span className="date">Issued May 2012</span>
-            </p>
-            <a className="btn-certificate" href="#">
-              <i
-                className="fas
+          <div className="course">
+            <img
+              className="course-logo"
+              src={plymouth}
+              alt="plymouth university"
+            />
+            <div className="course-content">
+              <h3>HND Applied Psychology</h3>
+              <p className="info">University of Plymouth</p>
+              <p className="grade">
+                <span className="date">Issued May 2012</span>
+              </p>
+              <a className="btn-certificate" href="#">
+                <i
+                  className="fas
               fa-graduation-cap"
-              ></i>
-              View Certificate
-            </a>
+                ></i>
+                View Certificate
+              </a>
+            </div>
           </div>
         </div>
       </div>
