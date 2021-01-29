cdd166ce9fb75ec2ccee1954b28661cf86a06cc0
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-29 16:19:06 +0000

    restructed about page

--- gatsbyConfig/siteMetadata/about.js
+++ gatsbyConfig/siteMetadata/about.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   aboutInfo: {
     image: {
-      src: "./images/profile.jpg",
+      src: `https://pbs.twimg.com/profile_images/1339602638111830020/k0dGL2Ye_400x400.png`,
       alt: "profile of Daniel Philip Johnson",
     },
     text: [
@@ -47,5 +47,105 @@ module.exports = {
     successfully joined codecareer.io as a react developer and
     future content creator.`,
     ],
+  },
+  // make icon function
+  aboutTechnologies: {
+    icons: [{ name: "Angular", alt: "" }],
+  },
+  quickBio: {
+    heading: "📔 Quick Biography",
+    html: `<p>
+  <b>When did I discover programming:</b> Well, when I was a young
+  child, my mother, in her spare time would copy code from a
+  manual to make games run on the <a href="https://en.wikipedia.org/wiki/Commodore_64">
+    Commodore 64 (CBM 64)
+  </a>
+  but putting them into memory. She kept this console for quite
+  some time it was the closest thing we had to a computer. The
+  first game I remember helping run was <a href="https://www.retrogamer.net/retro_games80/blue-meanies-from-outer-space/">
+    Blue Meanies from Outer Space
+  </a>
+  . It was so exciting to see it running, however, not so for my
+  mom. She claimed all that time for a space invaders clone.
+</p>
+
+<p>
+  Span a few years to one year before I started college. I needed
+  to buy a PC to submit my assignments. So during the summer of
+  2008, I worked so hard cleaning hotels with my mother. I now had
+  enough money to buy my first computer a Packard Bell iMedia
+  x2414 with an enormous amount of Ram totalling 2GB and an intel
+  Celeron with duo cores. The best part was it was running
+  everyone's favourite Windows version Windows Vista. It was so
+  hard to get any work done. Then one fatal evening windows broke
+  the DHCP no longer worked I had no clue how to fix it and no
+  other device to search how to fix it. I was then forced to use
+  the library to find a solution get a windows disc this seemed
+  almost impossible. I started to ask what is Windows and is there
+  an alternative. The answer was Yes! Ubuntu 9.04 (Jaunty
+  Jackalope). I encountered problems I was unable to download the
+  ISO. So I asked canonical for a disc and enclosed a cheque to
+  cover the shipping. From that day I have used Linux ever since.
+  The distros I commonly use now are "Kubuntu", "Manjaro" and Arch
+  Linux. I use KDE as my GUI "you know what they say old habits
+  die hard." Yes, I occasionally use Windows. After I built my
+  next PC, I got windows 7 and came across XNA Game Studio 3.0.
+  The idea of making games for Xbox seemed great. It gave me a
+  moment of nostalgia with my mom. Which lead me to learn C# and
+  eventually built an RPG game from the toolkit they provided. I
+  progressed onto making an Ubuntu Linux server out of interest as
+  well as a samba server. All in my free time. During my final
+  year of studies, I was responsible for creating experiments with
+  python and using libraries such as pandas, NumPy and matplotlib
+  to plot and analysis data. Now I found my calling with web
+  development and love every moment I get using JavaScript and
+  react.js
+</p>`,
+  },
+  valuesMissions: {
+    heading: "My values and mission",
+    html: `<p>
+    I always try to leave the world a better place than yesterday. I
+    try to give advice related to programming and personal. I always
+    try 99% of the time to help the best I can. If I don't know the
+    answer. I will try my hardest to find the answer or find another
+    person that can help you. I keep almost all my code 99.9% open
+    source for others to look at and use on GitHub(put a link). I
+    want to help you build apps, websites and programs that express
+    your ideas.
+  </p>
+  <p>
+    Currently, I'm finishing off my language studies with the
+    Spanish language. I know that speaking and reading in the
+    English language is a blessing. Many people do not have access
+    to good programming content due to it being in English.
+    Providing a language barrier. Also, most educational content is
+    not reasonably priced when you convert it to other currencies.
+    For instance, Peruvian Soles to Dollars it can make courses very
+    expensive. This is something I'm serious about addressing and
+    hoping to help with. That's why I volunteer my time and money to
+    help those from South America and North America. I want to also
+    make educational content that is accessible to as much of the
+    world as possible. If you would like to help by converting my
+    future tutorials, blog and books to other languages. Please let
+    me know :). I'm happy to give you credit. I hope we can all make
+    educational content more accessible to those who desire to
+    program can do it.
+  </p>`,
+  },
+  future: {
+    html: `Ways to support my journey, values and mission Follow Me You can
+    encourage me on Twitter, Instagram and LinkedIn. Tell me what
+    you need to know about web-development I would love to write
+    about it and help by making learning material. I want to create
+    content in the forms of blogs, posts and YouTube. Share, share
+    and share again! If you enjoy my content, please share it with
+    friends and family. Comment and give me valuable feedback! I
+    want my projects to be of high quality. Learn React or Vue.js
+    These are areas I specialise in so can answer your questions and
+    possibly help collaborate. Contribute Also, come join me on some
+    GitHub projects and practice with me! Find an issue with my
+    tutorial GitHub source let me know! Make a pull request and show
+    off your skills.`,
   },
 };
