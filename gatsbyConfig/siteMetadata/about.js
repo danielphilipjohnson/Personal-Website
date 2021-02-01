1e05e4e1e50e01e747a501a2d4da313b8d44b209
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-01 16:20:13 +0000

    added links to the text

--- gatsbyConfig/siteMetadata/about.js
+++ gatsbyConfig/siteMetadata/about.js
@@ -74,30 +74,32 @@ module.exports = {
   Span a few years to one year before I started college. I needed
   to buy a PC to submit my assignments. So during the summer of
   2008, I worked so hard cleaning hotels with my mother. I now had
-  enough money to buy my first computer a Packard Bell iMedia
-  x2414 with an enormous amount of Ram totalling 2GB and an intel
+  enough money to buy my first computer a <a href="https://www.mrmemory.co.uk/memory-ram-upgrades/packard-bell/imedia/x2414">Packard Bell iMedia
+  x2414</a> with an enormous amount of Ram totalling 2GB and an intel
   Celeron with duo cores. The best part was it was running
-  everyone's favourite Windows version Windows Vista. It was so
+  everyone's favourite Windows version <a href="https://en.wikipedia.org/wiki/Windows_Vista">Windows Vista</a>. It was so
   hard to get any work done. Then one fatal evening windows broke
-  the DHCP no longer worked I had no clue how to fix it and no
+  the <a href="https://social.technet.microsoft.com/Forums/windows/en-US/69029855-21c1-411d-bdd3-b864222eefb6/dhcp-on-vista-has-stopped-running?forum=itprovistasetup">DHCP no longer worked</a> I had no clue how to fix it and no
   other device to search how to fix it. I was then forced to use
   the library to find a solution get a windows disc this seemed
   almost impossible. I started to ask what is Windows and is there
-  an alternative. The answer was Yes! Ubuntu 9.04 (Jaunty
-  Jackalope). I encountered problems I was unable to download the
+  an alternative. The answer was Yes! <a href="https://wiki.ubuntu.com/JauntyJackalope">Ubuntu 9.04 (Jaunty
+  Jackalope)</a>. I encountered problems I was unable to download the
   ISO. So I asked canonical for a disc and enclosed a cheque to
   cover the shipping. From that day I have used Linux ever since.
-  The distros I commonly use now are "Kubuntu", "Manjaro" and Arch
-  Linux. I use KDE as my GUI "you know what they say old habits
+  The distros I commonly use now are <a href="https://kubuntu.org/">Kubuntu 20.10</a>, 
+  <a href="https://manjaro.org/">Manjaro"</a> and <a href="https://archlinux.org/">Arch
+  Linux</a>. I use <a href="https://kde.org/">KDE</a> as my GUI "you know what they say old habits
   die hard." Yes, I occasionally use Windows. After I built my
-  next PC, I got windows 7 and came across XNA Game Studio 3.0.
+  next PC, I got <a href="https://en.wikipedia.org/wiki/Windows_7">Windows 7</a> and came across <a href="https://en.wikipedia.org/wiki/Microsoft_XNA_Game_Studio"XNA Game Studio 3.0</a>.
   The idea of making games for Xbox seemed great. It gave me a
-  moment of nostalgia with my mom. Which lead me to learn C# and
-  eventually built an RPG game from the toolkit they provided. I
-  progressed onto making an Ubuntu Linux server out of interest as
+  moment of nostalgia with my mom. Which lead me to learn C# from <a href="https://docs.microsoft.com/en-us/learn/">Microsoft Academy</a> and
+  eventually built an <a href="https://sourceforge.net/projects/xnarpgtoolkit/">RPG game from the toolkit</a> they provided. I
+  progressed onto making an <a href="https://ubuntu.com/server">Ubuntu Linux server</a> out of interest as
   well as a samba server. All in my free time. During my final
   year of studies, I was responsible for creating experiments with
-  python and using libraries such as pandas, NumPy and matplotlib
+  python and using libraries such as <a href="https://pandas.pydata.org/">pandas<a/>, <a href="https://numpy.org/">NumPy</a> 
+  and <a href="https://matplotlib.org/">matplotlib<a/>
   to plot and analysis data. Now I found my calling with web
   development and love every moment I get using JavaScript and
   react.js`,
