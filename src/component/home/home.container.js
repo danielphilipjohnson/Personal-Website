6e649546955eb9847052195f7323a7a4b8ed727a
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-01 18:36:40 +0000

    refactor: removed index into a container

---
+++ src/component/home/home.container.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+import SocialIcons from "../../component/home/Icons";
+import HomeTools from "../../component/home/HomeTools";
+
+import Education from "../../component/home/Education";
+import Biography from "../../component/Biography";
+import Clients from "../../component/home/clients";
+import Services from "../../component/home/services";
+
+import bg from "../../images/showcase1.jpg";
+
+const HomeContainer = () => {
+  const { site } = useStaticQuery(siteQuery);
+  return (
+    <>
+      <section
+        id="home"
+        className="home"
+        style={{
+          backgroundImage: `url(${bg})`,
+        }}
+      >
+        <h1 className="lg-heading">
+          Daniel
+          <span id="secondary" className="text-secondary">
+            {" "}
+            Philip
+          </span>
+          <span> Johnson</span>
+        </h1>
+        <div id="icons" className="icons home-cta">
+          <h2 className="sm-heading">{site.siteMetadata.tagline}</h2>
+
+          <SocialIcons />
+        </div>
+      </section>
+
+      <HomeTools />
+      <Biography />
+      <Services />
+      <Clients />
+      <Education />
+    </>
+  );
+};
+
+export default HomeContainer;
+
+const siteQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        tagline
+      }
+    }
+  }
+`;
