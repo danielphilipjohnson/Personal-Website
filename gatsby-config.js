675e754776ce3f5d9a32da2424716bade5bcd8f7
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-01 17:49:44 +0000

    Split education data into its own sitemetadata file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 const projects = require("./gatsbyConfig/projects");
 const about = require("./gatsbyConfig/siteMetadata/about");
+const education = require("./gatsbyConfig/siteMetadata/education");
 
 const plugins = require("./gatsbyConfig/plugins");
 
@@ -105,86 +106,7 @@ module.exports = {
         },
       },
     ],
-    education: [
-      {
-        imageType: "uci",
-        title: "Project Management: The Basics for Success",
-        school: "University of California",
-        issueDate: "October 2020",
-        certificateLink:
-          "https://www.coursera.org/account/accomplishments/specialization/certificate/WKJ9ETHJFF7S",
-      },
-      {
-        imageType: "udemy",
-        title: "Modern HTML & CSS From The Beginning",
-        school: "Udemy",
-        issueDate: "September 2020",
-        certificateLink:
-          "https://www.udemy.com/certificate/UC-49df60d0-c960-433b-a62b-8bbaa5cd3846/",
-      },
-      {
-        imageType: "hong",
-        title: "Full-Stack Web Development with React",
-        school: "University of Hong Kong",
-        issueDate: "Aug 2020",
-        certificateLink:
-          "https://www.coursera.org/account/accomplishments/specialization/certificate/WKJ9ETHJFF7S",
-      },
-      {
-        imageType: "michigan",
-        title: "Web Applications for Everybody",
-        school: "University of Michigan",
-        issueDate: "June 2020",
-        certificateLink:
-          "https://www.coursera.org/account/accomplishments/specialization/certificate/EWWJFR4LJM7J",
-      },
-      {
-        imageType: "michigan",
-        title: "Python for Everybody",
-        school: "University of Michigan",
-        issueDate: "March 2020",
-        certificateLink:
-          "https://www.coursera.org/account/accomplishments/specialization/certificate/8UFSG6W97PMY",
-      },
-      {
-        imageType: "atlassian",
-        title: "Version Control with Git",
-        school: "Atlassian",
-        issueDate: "March 2020",
-        certificateLink:
-          "https://www.coursera.org/account/accomplishments/certificate/WCMWFJX9QAXV",
-      },
-      {
-        imageType: "google",
-        title: "Google IT Automation with Python Specialization",
-        school: "Google",
-        issueDate: "March 2020",
-        certificateLink:
-          "https://www.coursera.org/account/accomplishments/specialization/certificate/EEGGHB96R3KQ",
-      },
-      {
-        imageType: "michigan",
-        title: "Web Design for Everybody",
-        school: "University of Michigan",
-        issueDate: "May 2020",
-        certificateLink:
-          "https://www.coursera.org/account/accomplishments/specialization/certificate/2A36K26P5QER",
-      },
-      {
-        imageType: "plymouth",
-        title: "BSc Psychology",
-        school: "University of Plymouth",
-        issueDate: "April 2014",
-        certificateLink: "",
-      },
-      {
-        imageType: "plymouth",
-        title: "HND Applied Psychology",
-        school: "University of Plymouth",
-        issueDate: "May 2012",
-        certificateLink: "",
-      },
-    ],
+    education,
     projectTypes: ["All", "Js", "Responsive", "Angular", "Material"],
     projects: projects,
     about: about,
