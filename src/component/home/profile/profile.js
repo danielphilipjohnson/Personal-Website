78d3b9c6a0a37e708e5109ecaea803532d2998a5
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-16 12:49:35 +0000

    finished about me section

--- src/component/home/profile/profile.js
+++ src/component/home/profile/profile.js
@@ -1,37 +1,89 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
 
 import "./profile.css";
 
 function Profile() {
-  const {
-    site: { siteMetadata },
-  } = useStaticQuery(siteQuery);
   return (
-    <div className="container">
-      <div className="profile-text">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: siteMetadata.about.aboutInfo.html,
-          }}
-        ></div>
+    <section className="profile">
+      <div className="container">
+        <div className="profile-text">
+          <blockquote>
+            <p>
+              <span className="heading-2">Hi 👋 I am Daniel✨</span>,
+              <span className="text-highlight">
+                <strong>Front-End Engineer</strong>
+              </span>
+              with more than
+              <span className="text-highlight">
+                <strong>2+ years experience</strong>
+              </span>
+              building responsive and accessible web-applications and 6 months
+              experience in the startup world working remotely with
+              <a href="https://codecareer.teachable.com/">
+                <span className="text-highlight">
+                  <strong>codecareer.io</strong>
+                </span>
+              </a>
+              .
+            </p>
+
+            <p>
+              I primarly build front end views with the{" "}
+              <span className="text-highlight">
+                <strong>React</strong>
+              </span>{" "}
+              framework and use both{" "}
+              <span className="text-highlight">
+                <strong>Next.js</strong>
+              </span>{" "}
+              and
+              <span className="text-highlight">
+                <strong>Gatsby</strong>
+              </span>
+              . For styling I use
+              <span className="text-highlight">
+                <strong>TailwindCSS</strong>
+              </span>
+              , vanilla
+              <span className="text-highlight">
+                <strong>CSS</strong>
+              </span>{" "}
+              and on occassion
+              <span className="text-highlight">
+                <strong>Sass</strong>
+              </span>
+              for legacy projects. I dabble with some backend issues such as
+              MongoDB and mongoose schemas.
+            </p>
+            <p>
+              My responsibilities, include{" "}
+              <span className="text-highlight">
+                <strong>maintaining</strong>
+              </span>{" "}
+              <span className="emoji">🛠️</span> a job board hiring system for
+              junior developers. Currently, I am{" "}
+              <span className="text-highlight">
+                <strong>refactoring</strong>
+              </span>{" "}
+              the codebase and{" "}
+              <span className="text-highlight">
+                <strong>optimizing</strong>
+              </span>{" "}
+              load times <span className="emoji">⏱️</span>.
+            </p>
+            <p>
+              In my spare time, I am writing a TailwindCSS{" "}
+              <span className="emoji">📖</span> for a publisher as well as
+              writing a <span className="emoji">📖</span> about using Gatsby.js.
+              I also contribute to open source projects{" "}
+              <span className="emoji">✨</span>. and I am about to start a blog
+              series and data structures.
+            </p>
+          </blockquote>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
 export default Profile;
-const siteQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        tagline
-        about {
-          aboutInfo {
-            html
-          }
-        }
-      }
-    }
-  }
-`;
