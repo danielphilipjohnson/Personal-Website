406e56e78ba2e49a8ccc1bb4a7e3d3f6f088ae52
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-03-17 11:21:28 +0000

    cleaned work experience made more accessible

--- src/component/home/work-experience/work-experience.js
+++ src/component/home/work-experience/work-experience.js
@@ -6,7 +6,13 @@ function WorkExperience() {
   return (
     <section className="experience">
       <div className="container container-narrow">
-        <h2 className="text-purple skill-heading">👨‍💻 Experience:</h2>
+        <h2 className="text-purple skill-heading">
+          {" "}
+          <span role="img" aria-label="Daniel Johnson's">
+            👨‍💻
+          </span>{" "}
+          Experience:
+        </h2>
         <div className="job-meta">
           <span>November 2020 - Present</span>
         </div>
@@ -32,60 +38,107 @@ function WorkExperience() {
               <li>
                 I work as part of a
                 <span className="text-highlight">
-                  frontend development team
+                  <strong>frontend development team</strong>
                 </span>
                 implementing features in
-                <span className="text-highlight">ReactJS.</span> I also interact
-                with a designer by asking for feedback of completed work, and
-                informing what parts of the design might need to change to be
-                more accessible.
+                <span className="text-highlight">
+                  <strong>ReactJS.</strong>
+                </span>{" "}
+                I also interact with a designer by asking for feedback of
+                completed work, and informing what parts of the design might
+                need to change to be more accessible.
               </li>
               <li>
-                We we uses the <span className="text-highlight">agile</span>
-                methodology and <span className="text-highlight">jira</span>
+                We we uses the{" "}
+                <span className="text-highlight">
+                  <strong>agile</strong>
+                </span>
+                methodology and{" "}
+                <span className="text-highlight">
+                  <strong>jira</strong>
+                </span>
                 where developers are assigned features.
               </li>
 
               <li>
                 Daily, I use React.js,
-                <span className="text-highlight">Javascript</span> ECMA 5-11,
-                CSS, <span className="text-highlight">Tailwind CSS</span>
+                <span className="text-highlight">
+                  <strong>Javascript</strong>
+                </span>{" "}
+                ECMA 5-11, CSS,{" "}
+                <span className="text-highlight">
+                  <strong>Tailwind CSS</strong>
+                </span>
                 for styling and occasionally
-                <span className="text-highlight">Bootstrap 4</span>. I interact
-                with
-                <span className="text-highlight">express.js</span> routes, store
-                data in <span className="text-highlight">MongoDB</span> and use
-                ORM <span className="text-highlight">mongoose.</span> The
-                popular node packages we use is
-                <span className="text-highlight">form-hooks</span> and
-                <span className="text-highlight">Axios.</span>
+                <span className="text-highlight">
+                  <strong>Bootstrap 4</strong>
+                </span>
+                . I interact with
+                <span className="text-highlight">
+                  <strong>express.js</strong>
+                </span>{" "}
+                routes, store data in{" "}
+                <span className="text-highlight">
+                  <strong>MongoDB</strong>
+                </span>{" "}
+                and use ORM{" "}
+                <span className="text-highlight">
+                  <strong>mongoose.</strong>
+                </span>{" "}
+                The popular node packages we use is
+                <span className="text-highlight">
+                  <strong>form-hooks</strong>
+                </span>{" "}
+                and
+                <span className="text-highlight">
+                  <strong>Axios.</strong>
+                </span>
               </li>
 
               <li>
                 My responsibilities involve
                 <span className="text-highlight">
-                  maintaining the hiring system
+                  <strong>maintaining the hiring system</strong>
                 </span>
                 by
-                <span className="text-highlight">adding new features</span> and
-                <span className="text-highlight">updating job posts</span>.
-                Currently, I am
-                <span className="text-highlight">refactoring</span> parts of the
-                codebase, improving
-                <span className="text-highlight">responsiveness</span> by reduce
-                calling repaint and the time taken for the browser to render.
+                <span className="text-highlight">
+                  <strong>adding new features</strong>
+                </span>{" "}
+                and
+                <span className="text-highlight">
+                  <strong>updating job posts</strong>
+                </span>
+                . Currently, I am
+                <span className="text-highlight">
+                  <strong>refactoring</strong>
+                </span>{" "}
+                parts of the codebase, improving
+                <span className="text-highlight">
+                  <strong>responsiveness</strong>
+                </span>{" "}
+                by reduce calling repaint and the time taken for the browser to
+                render.
               </li>
 
               <li>
                 My recent work includes implementing front-end
-                <span className="text-highlight">CRUD</span>
+                <span className="text-highlight">
+                  <strong>CRUD</strong>
+                </span>
                 functionality for posting jobs to the job board, built a custom
                 hook for API requests,
-                <span className="text-highlight">updated SEO</span> and
-                favicons. My next goals are to
-                <span className="text-highlight">reduce API requests</span> with
-                <span className="text-highlight">local storage </span>and
-                implement infinite scrolling.
+                <span className="text-highlight">
+                  <strong>updated SEO</strong>
+                </span>{" "}
+                and favicons. My next goals are to
+                <span className="text-highlight">
+                  <strong>reduce API requests</strong>
+                </span>{" "}
+                with
+                <span className="text-highlight">
+                  <strong>local storage</strong>{" "}
+                </span>
+                and implement infinite scrolling.
               </li>
             </ul>
           </li>
@@ -109,7 +162,7 @@ function WorkExperience() {
               I interact with customers daily as well as coordinate with ASDA
               workers. I have almost{" "}
               <span className="text-highlight">
-                6 years of customer service skills
+                <strong>6 years of customer service skills</strong>
               </span>{" "}
               with previous jobs at Sainsbury's and Co-op.
             </p>
