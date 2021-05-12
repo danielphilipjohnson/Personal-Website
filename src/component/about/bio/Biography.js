3911b93d411def0b6977e1445352210bf0e0c724
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-05-12 15:19:37 +0100

    refactor: removed sitemetadata for bio

--- src/component/about/bio/Biography.js
+++ src/component/about/bio/Biography.js
@@ -3,7 +3,6 @@ import { useStaticQuery, graphql } from "gatsby";
 
 import "./biography.css";
 function Biography() {
-  const { site } = useStaticQuery(siteQuery);
   return (
     <div className="bio container">
       <div className="bio-content">
@@ -11,25 +10,44 @@ function Biography() {
           Mission Statement:
         </h2>
         <h4 className="bio-mission-heading">
-          {site.siteMetadata.biography.statement}
+          Mission driven react developer with a passion to create apps that
+          allows you to express your ideas.
         </h4>
 
         <p className="bio-content">
-          {site.siteMetadata.biography.missionStatement}
+          Enthusiastic programmer looking to expand his skills with the right
+          front-end react developer position. I have a Theoretical knowledge of
+          the fundamentals HTML, CSS and JS. I specialise in react development,
+          graphql, redux, redux thunk and reselect. I'm seeking to further
+          develop my front end skills and be part of a team that can accelerate
+          my development.
         </p>
         <p className="bio-content">
-          {site.siteMetadata.biography.subStatement}
+          I started with web development during my years at univeristy. I made
+          websites and 2D games as a hobby. In my 3rd year I was responsible for
+          creating programs for neuroscience lab projects. Recently I started to
+          pursue web development as a career because it is something I enjoy. I
+          enrolled with coursera and completed courses, im currently updating my
+          freecodecamp projects and improving my skills with Udemy courses.
         </p>
         <p className="bio-content">
           Current Focus:
-          {site.siteMetadata.biography.focuses.map((focus, i) => {
-            return (
-              <span key={focus + i}>
-                <span> {"//"} </span>
-                <span className="text-purple-dark "> {focus} </span>
-              </span>
-            );
-          })}
+          <span>
+            <span> {"//"} </span>
+            <span className="text-purple-dark "> Accessibility </span>
+          </span>
+          <span>
+            <span> {"//"} </span>
+            <span className="text-purple-dark "> React </span>
+          </span>
+          <span>
+            <span> {"//"} </span>
+            <span className="text-purple-dark "> Performance </span>
+          </span>
+          <span>
+            <span> {"//"} </span>
+            <span className="text-purple-dark "> Responsive </span>
+          </span>
         </p>
       </div>
     </div>
@@ -37,18 +55,3 @@ function Biography() {
 }
 
 export default Biography;
-
-const siteQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        biography {
-          statement
-          missionStatement
-          subStatement
-          focuses
-        }
-      }
-    }
-  }
-`;
