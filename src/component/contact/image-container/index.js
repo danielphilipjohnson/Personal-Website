a58e4eafd19c9c2ccd19acb98c3229e7d7454bf8
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-05-15 18:20:24 +0100

    issue: links dont have discernible names.

--- src/component/contact/image-container/index.js
+++ src/component/contact/image-container/index.js
@@ -30,36 +30,54 @@ function ImageContainer() {
 
         <div className="boxes">
           <div className="box dev">
-            <a href="https://dev.to/danielphilipjohnson">
+            <a
+              href="https://dev.to/danielphilipjohnson"
+              aria-label="dev blog account"
+            >
               <FontAwesomeIcon icon={faDev} />
             </a>
           </div>
 
           <div className="box hackerrank">
-            <a href="https://www.hackerrank.com/daniel_p_johnson">
+            <a
+              href="https://www.hackerrank.com/daniel_p_johnson"
+              aria-label="hacker rank"
+            >
               <FontAwesomeIcon icon={faHackerrank} />
             </a>
           </div>
 
           <div className="box codepen">
-            <a href="https://codepen.io/danielphilipjohnson">
+            <a
+              href="https://codepen.io/danielphilipjohnson"
+              aria-label="codepen"
+            >
               <FontAwesomeIcon icon={faCodepen} />
             </a>
           </div>
 
           <div className="box stack-overflow">
-            <a href="https://stackoverflow.com/users/13921677/daniel-philip-johnson">
+            <a
+              href="https://stackoverflow.com/users/13921677/daniel-philip-johnson"
+              aria-label="stack overflow"
+            >
               <FontAwesomeIcon icon={faStackOverflow} />
             </a>
           </div>
 
           <div className="box free-code-camp">
-            <a href="https://www.freecodecamp.org/daniel-philip-johnson">
+            <a
+              href="https://www.freecodecamp.org/daniel-philip-johnson"
+              aria-label="free code camp"
+            >
               <FontAwesomeIcon icon={faFreeCodeCamp} />
             </a>
           </div>
           <div className="box github">
-            <a href="https://github.com/danielphilipjohnson">
+            <a
+              href="https://github.com/danielphilipjohnson"
+              aria-label="github"
+            >
               <FontAwesomeIcon icon={faGithub} />
             </a>
           </div>
