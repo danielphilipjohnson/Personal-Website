208383c82c910766074a9d24dc17ad651b153bd9
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-05-15 21:08:41 +0100

    enhancement: made image more responsive and reduced filesize

--- src/component/about/about-card/index.js
+++ src/component/about/about-card/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
 import PDF from "../../../../downloads/Daniel_Philip_Johnson_Front_End_Engineer.pdf";
-import banner from "../../../images/linkedinBanner.png";
+// import banner from "../../../images/linkedinBanner.png";
 
 function AboutCard() {
   return (
